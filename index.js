5f29aaa59047fcf0ffa4910a6ac6ecf0c0e48e88
Makarenko Ivan <ivan.makarenko@free.fr>
2022-07-25 18:28:37 +0200

    Slider page d'accueil automatique

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ toggle_menu.addEventListener('click', () => {
   toggle_menu.classList.toggle('active') ;
   menu.classList.toggle('responsive') ;
 })
+
+
+// Slider
+let img__slider = document.getElementsByClassName('img__slider');
+
+let etape = 0;
+
+let nbr__img = img__slider.length;
+
+function enleverActiveImages() {
+  for(let i = 0; i < nbr__img; i++) {
+    img__slider[i].classList.remove('activer');
+  }
+}
+
+setInterval(function () {
+  etape ++;
+  if(etape >= nbr__img) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  img__slider[etape].classList.add('activer');
+}, 7000)
