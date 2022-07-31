ac83337ab09f98082e416513f484ec59fef3cd69
Makarenko Ivan <ivan.makarenko@free.fr>
2022-07-31 17:49:13 +0200

    Slide Toggle Down

--- gestes.js
+++ gestes.js
@@ -6,3 +6,23 @@ toggle_menu.addEventListener('click', () => {
   toggle_menu.classList.toggle('active') ;
   menu.classList.toggle('responsive') ;
 })
+
+
+// Slide Toggle Down
+
+var quoteContainer = document.querySelectorAll(".quote-container");
+var down = document.getElementsByClassName("arrowIcon");
+var appart = document.querySelectorAll(".appart");
+
+let fleche = quoteContainer.length;
+
+function enleverActiveImages() {
+  for(let i = 0; i < fleche; i++) {
+    quoteContainer[i].addEventListener('click', () => {
+      down[i].classList.toggle("rotate");
+      appart[i].classList.toggle("apparition");
+    })
+  }
+}
+
+enleverActiveImages();
