d3e53760317ab20ea90dc78a48994817ddf41f13
Makarenko Ivan <ivan.makarenko@free.fr>
2022-08-03 19:40:36 +0200

    Card reglement + onglet

--- don.js
+++ don.js
@@ -35,6 +35,31 @@ function tabsAnimation(e){
     
 }
 
+const payar = Array.from(document.querySelectorAll(".paiement")); // La méthode Array.from() permet de créer une nouvelle instance d'Array (une copie superficielle) à partir d'un objet itérable ou semblable à un tableau.
+const reglementation = Array.from(document.querySelectorAll(".mon-reglement"));
+
+
+payar.forEach(payars => {
+  payars.addEventListener("click", payarAnimation)
+})
+
+let payer = 0;
+
+function payarAnimation(e){
+
+    const el = e.target;
+    
+    payar[payer].classList.remove("paiement-activer");
+    reglementation[payer].classList.remove("mon-reglement-activer");
+    
+    payer = payar.indexOf(el);
+    
+    payar[payer].classList.add("paiement-activer")
+    reglementation[payer].classList.add("mon-reglement-activer");
+    
+}
+
+
 // Button color
 
 const button = Array.from(document.querySelectorAll(".button-color")); 
